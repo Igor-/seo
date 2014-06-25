d46e11181cf87a4a2e0abb946d4141a45bde82e2
Oleg Bovykin <oleg.bovykin@gmail.com>
2014-06-25 12:07:19 +0400

    Added truncations in views

---
+++ app/helpers/redde/seo/seos_helper.rb
@@ -0,0 +1,28 @@
+module Redde::Seo::SeosHelper
+  def seo_link_for_url(seo)
+    link_to seo_url_for(seo), [:edit, :admin, seo], alt: seo.url, title: seo.url
+  end
+
+  def seo_url_for(seo)
+    title = truncate(seo.url, length: 17)
+    title.present? ? title : 'НЕ ЗАПОЛНЕНО'
+  end
+
+  def seo_link_for_object(seo)
+    link_to seo_object_for(seo), [:edit, :admin, seo], alt: seo.url, title: seo.url
+  end
+
+  def seo_object_for(seo)
+    return '' unless seo.seoable
+    "#{seo.seoable.class.model_name.human}##{seo.seoable_id}"
+  end
+
+  def seo_title_for(seo)
+    title = seo.title.present? ? seo.title : 'НЕ ЗАПОЛНЕНО'
+    link_to title, [:edit, :admin, seo], alt: seo.url, title: seo.url
+  end
+
+  def seo_empty_for(seo)
+    seo.empty? ? '✘' : '✔'
+  end
+end
--- lib/redde_seo/engine.rb
+++ lib/redde_seo/engine.rb
@@ -1,4 +1,9 @@
 module ReddeSeo
   class Engine < ::Rails::Engine
+    initializer 'redde_seo.action_controller' do |_app|
+      ActiveSupport.on_load :action_controller do
+        helper Redde::Seo::SeosHelper
+      end
+    end
   end
 end
